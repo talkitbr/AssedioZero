ea1a0ccc662a5a61bc62aa3434a51f080d1823f7
Patrick Tedeschi <patrick.tedeschi@eldorado.org.br>
2016-06-10 16:50:47 -0300

    Initial version

---
+++ source/AssedioZero/www/scripts/constants.js
@@ -0,0 +1,44 @@
+﻿// Azure mobile app URL
+var azureMobileApp = "<AZURE_MOBILE_APP>";
+
+// Azure mobile app Easy Table name
+var azureTableName = "HARASSMENT"; // App Assédio Zero
+
+// Row limit to return 
+var rowLimit = 50;
+
+// Google pin url
+var googlePinUrl = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+
+
+// ---------------------------- Text values constants ---------------------------- 
+
+// App name
+var appName = "Assédio Zero";
+
+// App type
+var appType = "assédio"; // App Assédio Zero
+
+// Physical text
+var physicalValue = "Físico"; // App Assédio Zero
+
+// Verbal Text
+var verbalValue = "Verbal";
+
+// Confirm dialog message text
+var dialogConfirmMessage = "Confirmar envio de " + appType + " ";
+
+// Sent to azure table with success text
+var sentSuccessMessage = "Ponto de " + appType + " enviado"
+
+// Error message
+var errorMessage = "Ocorreu um erro no processamento da sua requisição. Tente novamente mais tarde.";
+
+// No Internet message
+var noInternetMessage = "Por favor verifique sua conexão com a internet.";
+
+// Send data progress text
+var sendDataLoadingText = "Enviando ponto de assédio...";
+
+// Loading map data progress text
+var loadingMapText = "Carregando informações no mapa...";
